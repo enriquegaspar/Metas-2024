7a1ae86b66d25310ddb5cc60956f876089d2654f
enriquegaspar <105357875+enriquegaspar@users.noreply.github.com>
2024-04-05 08:33:31 -0300

    ok

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = calculaTempo;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo1){
 let tempoAtual = new date ();
 let tempoFinal = tempoObjetivo1 - tempoAtual;
