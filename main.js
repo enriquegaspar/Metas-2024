1dab24c111dfea5fb7af58f1a6ee9db09825b2ab
enriquegaspar <105357875+enriquegaspar@users.noreply.github.com>
2024-04-12 08:30:42 -0300

    ok

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const tempoObjetivo3 = new Date("2024-12-8T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3];
 
-
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
