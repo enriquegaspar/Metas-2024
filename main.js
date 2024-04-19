03e8e6a2bc9413089dea155a59e994deb9d84b20
enriquegaspar <105357875+enriquegaspar@users.noreply.github.com>
2024-04-19 08:30:42 -0300

    ok

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-22T00:00:00");
 const tempoObjetivo2 = new Date("2024-07-15T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-8T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-08T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3];
 
