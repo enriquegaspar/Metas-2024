6c1dec36ee6f5a7e1807dcb447011a6829afd767
enriquegaspar <105357875+enriquegaspar@users.noreply.github.com>
2024-05-10 08:16:59 -0300

    ok

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ minutos %= 60;
 horas %= 24;
 
 if (tempoFinal > 0){
-return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }else{
     return "PRAZO ENCERRADO!!!"
 }
